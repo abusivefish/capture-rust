0f46fcf457dad43423531ea7111f2052e8942b32
Connor Law <connor.claw@gmail.com>
2020-01-28 21:42:51 -0700

    engage

---
+++ src/sources/tc358840xbg.rs
@@ -0,0 +1,13 @@
+// tc358743xbg
+// 1920x1080, 60 fps, 24 bpp
+// Supported color spaces -
+//      MEDIA_BUS_FMT_UYVY8_1X16: YCbCr422
+//      MEDIA_BUS_FMT_RGB888_1X24: YCbCr444
+// see: https://toshiba.semicon-storage.com/info/docget.jsp?did=35655&prodName=TC358743XBG
+
+
+// THIS IS NOT SUPPORTED BY LINUX YET
+// tc358840xbg
+// 3840x2160, 30fps, 24bpp
+// 
+// see: https://toshiba.semicon-storage.com/info/docget.jsp?did=28738&prodName=TC358840XBG
